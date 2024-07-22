db86ec7dfd20a717abc805adaaa723f6cbe733a1
Jorge Revuelta <jrjrevuelta@gmail.com>
2024-07-22 09:50:21 +0200

    env files done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './tasks/task.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/task.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb+srv://jrjrevuelta:7qq5Esq4Vf3nKtb0@tasks.qb3nbco.mongodb.net/?retryWrites=true&w=majority&appName=Tasks', //later with env file
-      database: 'Tasks', 
-      synchronize: true,
-      useUnifiedTopology: true,
-      entities: [Task],
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get<string>('DATABASE_URL'),
+        database: 'Tasks',
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [Task],
+      }),
+      inject: [ConfigService],
     }),
     TasksModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  
   app.enableCors({
-    origin: 'http://localhost:5173', // URL del frontend (later with env file)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
+    origin: configService.get('FRONTEND_URL'),
   });
+
   await app.listen(3000);
 }
 bootstrap();
